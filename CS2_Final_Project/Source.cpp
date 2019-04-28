4aea54e0ca3c5d2c614748382ddffcba4f23acb5
Kent Phan <kentphan2000@gmail.com>
2019-04-28 03:50:08 -0700

    Added a comment to source

--- CS2_Final_Project/Source.cpp
+++ CS2_Final_Project/Source.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
 	cout << "Hello world\n;";
-	
+	cout << "fWKEIRHMJIWE\n";
+	cout << "Hello world\n;";
+	cout << "fWKEIRHMJIWE\n";
+	cout << "Hello world\n;";
+	cout << "fWKEIRHMJIWE\n";
 	return 0;
 }
