3aa90fd33631fe76f33a00780129ed272b2dae38
Kent Phan <kentphan2000@gmail.com>
2019-05-01 17:35:45 -0700

    Added 2d for board and color to text (Experimental)

--- CS2_Final_Project/Source.cpp
+++ CS2_Final_Project/Source.cpp
@@ -1,23 +1,51 @@
+// color your text in Windows console mode
+// colors are 0=black 1=blue 2=green and so on to 15=white  
+// colorattribute = foreground + background * 16
+// to get red text on yellow use 4 + 14*16 = 228
+// light red on yellow would be 12 + 14*16 = 236
+// a Dev-C++ tested console application by  vegaseat  07nov2004
 #include <iostream>
- 
-using namespace std;
+#include <windows.h>   // WinApi header
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;    // std::cout, std::cin
 
 int main()
 {
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	cout << "Hello world\n;";
-	cout << "fWKEIRHMJIWE\n";
-	
-	// Hello
+	HANDLE  hConsole;
+	int k;
+
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	// you can loop k higher to see more color choices
+	// pick the colorattribute k you want
+
+	k = 12; // red = 12 
+	k = 14; // yellow = 14
+
+	SetConsoleTextAttribute(hConsole, k);
+
+	char board[6][7] =
+	{
+		{'o','o','o','o','o','o','o'},
+		{'o','o','o','o','o','o','o'},
+		{'o','o','o','o','o','o','o'},
+		{'o','o','o','o','o','o','o'},
+		{'o','o','o','o','o','o','o'},
+		{'o','o','o','o','o','o','o'}
+	};
+
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			if (j == 6)
+				cout << board[i][j] << endl;
+			else
+				cout << board[i][j] << " ";
+		}
+	}
 
 	return 0;
 }
+
